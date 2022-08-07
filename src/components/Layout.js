8f0215d7f4dc11605a15e252c2aa2457cf5b0d64
Akib <akibahmed2011@gmail.com>
2022-08-08 01:36:53 +0600

    menu added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
 const Layout = (props) => {
-  return <div>{props.children}</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <main>{props.children}</main>
+    </React.Fragment>
+  );
 };
 
 export default Layout;
