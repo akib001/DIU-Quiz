0637b55288b2db698d2dc71714e874a8733b7354
akib <akibahmed2011@gmail.com>
2022-08-08 15:29:55 +0600

    header routing added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import CreateQuiz from "./pages/CreateQuiz";
+import AdminHeader from "./components/admin/AdminHeader";
+import AdminDashboard from "./components/admin/AdminDashboard";
 
 function App() {
   return (
-    <Layout>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
+        <Route element={<AdminHeader/>}>
+          <Route path="/create-quiz" element={<CreateQuiz/>} />
+          <Route path="/dashboard" element={<AdminDashboard/>} />
+        </Route>
         <Route path="/create-quiz" element={<CreateQuiz />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Layout>
   );
 }
 
